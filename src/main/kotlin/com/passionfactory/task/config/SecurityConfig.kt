f9adbf89555c89a86d472b88719fea5a82334536
Yonhgui <yh3488@adwitt.com>
2021-11-29 22:05:18 +0900

    fix simple authorization

--- src/main/kotlin/com/passionfactory/task/config/SecurityConfig.kt
+++ src/main/kotlin/com/passionfactory/task/config/SecurityConfig.kt
@@ -4,18 +4,19 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http
             .httpBasic().disable()
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/todos", "/todos/*").permitAll()
-            .anyRequest().authenticated()
+            .anyRequest().permitAll()
             .and()
             .addFilterBefore(AuthorizeFilter(), UsernamePasswordAuthenticationFilter::class.java)
     }
